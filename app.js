78577a4ae0a9ae176817b92962d99d7325e8ef8b
furqanalfath <malvouz@gmail.com>
2023-05-26 13:48:26 +0700

    resign

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Udah beres ni belajar dari sini <a href="https://aws.amazon.com/getting-started/learning-path-devops-engineer/">https://aws.amazon.com/getting-started/learning-path-devops-engineer/</a>'));
+app.get('/', (req, res) => res.send('Udah beres ni belajar dari sini <a href="https://aws.amazon.com/getting-started/learning-path-devops-engineer/">https://aws.amazon.com/getting-started/learning-path-devops-engineer/</a> </br> resign ni gue dari Emerson wkwkwkw'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
